b6ea0681b5862ffcc2907dbca7d6873c9a1a7cfc
NormalReedus <magnus.borregaard@gmail.com>
2022-03-14 23:19:20 +0100

    router, default proxies, start caching mw

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,26 +1,33 @@
 package config
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/flynn/json5"
 )
 
 func Load(configPath string) *Config {
-
 	jsonFile, err := os.Open(configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer jsonFile.Close()
 
-	jsonByteValue, _ := ioutil.ReadAll(jsonFile)
+	jsonByteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	var config = &Config{}
 
 	json5.Unmarshal(jsonByteValue, &config)
+
+	config.trimTrailingSlash()
 
 	return config
 }
@@ -29,7 +36,19 @@ type Config struct {
 	MaxSize int      `json:"maxSize"`
 	ApiUrl  string   `json:"apiUrl"`
 	Cache   []string `json:"cache"`
-	Bust    BustMap
+	Bust    BustMap  `json:"bust"`
+}
+
+func (conf *Config) trimTrailingSlash() {
+	conf.ApiUrl = strings.TrimSuffix(conf.ApiUrl, "/")
+}
+
+func (conf Config) String() string {
+	confJSON, err := json.MarshalIndent(conf, "", "  ")
+	if err != nil {
+		fmt.Println("there was an issue printing the configuration")
+	}
+	return string(confJSON)
 }
 
 type BustMap struct {
@@ -37,6 +56,6 @@ type BustMap struct {
 	HEAD   map[string][]string `json:"HEAD"`
 	POST   map[string][]string `json:"POST"`
 	PUT    map[string][]string `json:"PUT"`
-	DELETE map[string][]string `json:"DELETE"`
 	PATCH  map[string][]string `json:"PATCH"`
+	DELETE map[string][]string `json:"DELETE"`
 }
--- main.go
+++ main.go
@@ -2,10 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/NormalReedus/lru-cache-microservice/internal/config"
+	"github.com/NormalReedus/lru-cache-microservice/internal/router"
+	flag "github.com/spf13/pflag"
 )
 
+const DEFAULT_CONFIG_PATH = "./cache-config.json5"
+
 func main() {
-	fmt.Println(config.Load("./config.example.json5"))
+	var configPath, port string = parseArgs()
+
+	conf := config.Load(configPath)
+
+	router.Start(conf, port)
+}
+
+func parseArgs() (string, string) {
+	helpPtr := flag.BoolP("help", "h", false, "Print help.")
+	portPtr := flag.StringP("port", "p", "3000", "The port to serve the service on.")
+	flag.Parse()
+
+	if *helpPtr {
+		printHelp()
+		os.Exit(0)
+	}
+
+	// Grab config file path from first arg given through CLI
+	configPath := flag.Arg(0)
+
+	// If no config path is given from CLI, use default
+	if configPath == "" {
+		configPath = DEFAULT_CONFIG_PATH
+	}
+
+	return configPath, *portPtr
+}
+
+func printHelp() {
+	fmt.Print("lru-cache-microservice is a reverse proxy for caching simple requests to a REST API. You only have to configure your API to trust this proxy for optimal conditions.\n\n")
+	fmt.Println("When running lru-cache-microservice, you will need to supply the programme with a configuration file (JSON/JSON5) that specifies which requests to cache, and when to bust the cache.")
+	fmt.Println("You can find detailed documentation on how to use lru-cache-microservice at https://github.com/NormalReedus/lru-cache-microservice/blob/main/README.md.")
 }
